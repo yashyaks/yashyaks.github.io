ac7072ba7563502e6e4b3c1a75d0061c2633cf1a
Yash Thakar <90375840+yashyaks@users.noreply.github.com>
2025-02-22 14:45:37 +0530

    hi

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,20 +16,23 @@ export default function Hero() {
         <h1 className="border-b-2 border-b-gray text-2xl font-semibold sm:text-3xl mb-6">
         About Me
         </h1>
-        <section className='mb-6'>
+        <section className='mb-6 space-y-4'>
           <h2 className="font-medium text-lg sm:text-xl text-blue-600">
-            Hello I am Yash Thakar <span className="text-2xl">👋</span>
+            Hello! I am Yash Thakar <span className="text-2xl">👋,</span>
           </h2>
-          <p className="font-normal text-gray-600 leading-loose">
-          Lorem ipsum odor amet, consectetuer adipiscing elit. Varius neque porta nascetur finibus, lorem turpis. Arcu vehicula eleifend nibh, lacinia accumsan hendrerit? Viverra scelerisque porttitor praesent malesuada sed purus justo praesent proin. At cursus sodales diam, leo dignissim ornare. Sem magna euismod himenaeos semper facilisi. Nam ac consequat, ut class platea et. Senectus lacinia dis posuere nisl ac donec aenean.
-          </p>
-          <p className="font-normal text-gray-600 leading-loose">
-          At turpis est senectus consectetur ipsum turpis. Et ridiculus viverra mattis sem nisl libero phasellus. Imperdiet mattis in; suscipit augue dis viverra cras. Vestibulum dapibus turpis morbi mauris amet placerat maximus. Interdum ultrices mollis suspendisse sit nascetur; adipiscing donec vulputate. Lacus dictum bibendum platea fusce eleifend. Imperdiet nascetur mattis dignissim ultrices nascetur gravida sem.
-          </p>
-          <p className="font-normal text-gray-600 leading-loose">
-          Semper hendrerit lacus montes dignissim arcu. Venenatis mus in dui vehicula venenatis sagittis. Feugiat magnis sodales magna pulvinar quis. Tellus tempus venenatis habitasse rutrum facilisi bibendum porta pretium id. Nostra ultrices ipsum lacus consequat natoque sagittis senectus diam. Parturient condimentum feugiat varius sodales neque leo. Donec ipsum imperdiet lobortis maximus velit.
-          </p>
 
+            <p className="font-normal text-gray-600 leading-8">
+            I am a passionate and driven Final Year Computer Science & Engineering student with a specialization in Data Science at DJSCE, University of Mumbai. I have continuously pushed myself to explore the vast realms of Artificial Intelligence, Machine Learning, and Data Engineering always striving to stay at the cutting edge of technology.
+            </p>
+            <p className="font-normal text-gray-600 leading-8">
+            Over the past few years, I’ve had the opportunity to intern at leading companies, gaining invaluable hands-on experience in both AI/ML and Data Engineering. I am proficient in various programming languages such as Python, SQL, and C, and have a deep understanding of Machine Learning frameworks like TensorFlow and PyTorch. I have experience in handling large datasets using PySpark, conducting data visualizations with PowerBI, Tableau, and Matplotlib, and developing scalable applications with Django, Flask, and MySQL. I’m well-versed in cloud services, particularly AWS, where I’ve utilized tools like S3, Lambda, and EC2 to build robust and cost-efficient solutions.
+            </p>
+            <p className="font-normal text-gray-600 leading-8">
+            I’ve had the privilege of serving as the Chairperson of DJS-Compute, leading a team of 20+ experienced mentors to guide over 60 students in building projects in Machine Learning, Web Development, and App Development. I’ve also had the honor of serving as an Academic Mentor at the We Can We Will Foundation, helping underprivileged students achieve their academic goals.
+            </p>
+            <p className="font-normal text-gray-600 leading-8">
+            I am always eager to collaborate on challenging and impactful projects. Whether it's AI, Machine Learning or Data engineering, I am enthusiastic about contributing to the tech community while continually expanding my knowledge. Feel free to reach out via LinkedIn or check out my projects on GitHub!
+            </p>
         </section>
       </main>
     </div>
